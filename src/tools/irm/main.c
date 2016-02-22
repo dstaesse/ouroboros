9fb8dff10efc9869264e55da64b64c4aadf805a6
Sander Vrijders <sander.vrijders@intec.ugent.be>
2016-02-22 17:34:21 +0100

    src: Rename daemons and add tools folder
    This renames the daemons to end with a 'd', as is common for UNIX
    daemons. It also adds a tools folder, that will hold the tools of the
    Ouroboros prototype. Examples are a simple echo application, an
    application to instruct the IRM ...

---
+++ src/tools/irm/main.c
@@ -0,0 +1,32 @@
+/*
+ * Ouroboros - Copyright (C) 2016
+ *
+ * A tool to instruct the IRM
+ *
+ *    Sander Vrijders <sander.vrijders@intec.ugent.be>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#define OUROBOROS_PREFIX "irm"
+
+#include <ouroboros/logs.h>
+
+int main () {
+
+        LOG_DBG("Test of the IRM tool");
+
+        return 0;
+}
