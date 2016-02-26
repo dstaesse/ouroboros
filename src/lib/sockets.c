c280c375ca6987f3ee8f6856b4c4c445efa7f6a4
Sander Vrijders <sander.vrijders@intec.ugent.be>
2016-02-26 16:12:46 +0100

    lib: Add identifier for non-literal string
    This adds the %s identifier in a sprintf. A non-literal was passed and
    used as a literal.

--- src/lib/sockets.c
+++ src/lib/sockets.c
@@ -79,7 +79,7 @@ int server_socket_open(char * file_name)
         }
 
         serv_addr.sun_family = AF_UNIX;
-        sprintf(serv_addr.sun_path, file_name);
+        sprintf(serv_addr.sun_path, "%s", file_name);
 
         if (bind(sockfd,
                  (struct sockaddr *) &serv_addr,
