9bca11727451f364f180758b46051a8e0398bccd
Sander Vrijders <sander.vrijders@intec.ugent.be>
2016-03-14 10:30:55 +0100

    lib: Move list function definitions into a source file
    This moves the function definitions of list.h into a separate source
    file to avod linker errors when including the file in more than one
    source file.

--- include/ouroboros/list.h
+++ include/ouroboros/list.h
@@ -49,22 +49,6 @@ struct list_head {
                 (ptr)->next = (ptr); (ptr)->prev = (ptr);       \
         } while (0)
 
-/*
- * Insert a new entry between two known consecutive entries.
- *
- * This is only for internal list manipulation where we know
- * the prev/next entries already!
- */
-static void __list_add(struct list_head * new,
-                       struct list_head * prev,
-                       struct list_head * next)
-{
-        next->prev = new;
-        new->next = next;
-        new->prev = prev;
-        prev->next = new;
-}
-
 /**
  * list_add - add a new entry
  * @new: new entry to be added
@@ -74,10 +58,7 @@ static void __list_add(struct list_head * new,
  * This is good for implementing stacks.
  */
 void list_add(struct list_head * new,
-              struct list_head * head)
-{
-        __list_add(new, head, head->next);
-}
+              struct list_head * head);
 
 /**
  * list_add_tail - add a new entry
@@ -88,24 +69,7 @@ void list_add(struct list_head * new,
  * This is useful for implementing queues.
  */
 void list_add_tail(struct list_head * new,
-                   struct list_head * head)
-{
-        __list_add(new, head->prev, head);
-}
-
-/*
- * Delete a list entry by making the prev/next entries
- * point to each other.
- *
- * This is only for internal list manipulation where we know
- * the prev/next entries already!
- */
-static void __list_del(struct list_head * prev,
-                       struct list_head * next)
-{
-        next->prev = prev;
-        prev->next = next;
-}
+                   struct list_head * head);
 
 /**
  * list_del - deletes entry from list.
@@ -113,22 +77,13 @@ static void __list_del(struct list_head * prev,
  * Note: list_empty on entry does not return true after this,
  * the entry is in an undefined state.
  */
-void list_del(struct list_head * entry)
-{
-        __list_del(entry->prev, entry->next);
-        entry->next = (void *) 0;
-        entry->prev = (void *) 0;
-}
+void list_del(struct list_head * entry);
 
 /**
  * list_del_init - deletes entry from list and reinitialize it.
  * @entry: the element to delete from the list.
  */
-void list_del_init(struct list_head * entry)
-{
-        __list_del(entry->prev, entry->next);
-        INIT_LIST_HEAD(entry);
-}
+void list_del_init(struct list_head * entry);
 
 /**
  * list_move - delete from one list and add as another's head
@@ -136,11 +91,7 @@ void list_del_init(struct list_head * entry)
  * @head: the head that will precede our entry
  */
 void list_move(struct list_head * list,
-               struct list_head * head)
-{
-        __list_del(list->prev, list->next);
-        list_add(list, head);
-}
+               struct list_head * head);
 
 /**
  * list_move_tail - delete from one list and add as another's tail
@@ -148,34 +99,13 @@ void list_move(struct list_head * list,
  * @head: the head that will follow our entry
  */
 void list_move_tail(struct list_head * list,
-                    struct list_head * head)
-{
-        __list_del(list->prev, list->next);
-        list_add_tail(list, head);
-}
+                    struct list_head * head);
 
 /**
  * list_empty - tests whether a list is empty
  * @head: the list to test.
  */
-int list_empty(struct list_head * head)
-{
-        return head->next == head;
-}
-
-static void __list_splice(struct list_head *list,
-                          struct list_head *head)
-{
-        struct list_head *first = list->next;
-        struct list_head *last = list->prev;
-        struct list_head *at = head->next;
-
-        first->prev = head;
-        head->next = first;
-
-        last->next = at;
-        at->prev = last;
-}
+int list_empty(struct list_head * head);
 
 /**
  * list_splice - join two lists
@@ -183,11 +113,7 @@ static void __list_splice(struct list_head *list,
  * @head: the place to add it in the first list.
  */
 void list_splice(struct list_head * list,
-                 struct list_head * head)
-{
-        if (!list_empty(list))
-                __list_splice(list, head);
-}
+                 struct list_head * head);
 
 /**
  * list_splice_init - join two lists and reinitialise the emptied list.
@@ -197,13 +123,7 @@ void list_splice(struct list_head * list,
  * The list at @list is reinitialised
  */
 void list_splice_init(struct list_head * list,
-                      struct list_head * head)
-{
-        if (!list_empty(list)) {
-                __list_splice(list, head);
-                INIT_LIST_HEAD(list);
-        }
-}
+                      struct list_head * head);
 
 /**
  * list_entry - get the struct for this entry
