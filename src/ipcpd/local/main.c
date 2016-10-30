4fb81107bfad2fe4cbea00cb87e1b6fbb92bd869
dimitri staessens <dimitri.staessens@intec.ugent.be>
2016-10-30 22:33:17 +0100

    ipcpd: Clean shutdown when bootstrap fails
    Fixes a double free (conf->dif_name) and the handling of non-created
    threads when the bootstrap of an IPCP fails

--- src/ipcpd/local/main.c
+++ src/ipcpd/local/main.c
@@ -140,7 +140,11 @@ void ipcp_sig_handler(int sig, siginfo_t * info, void * c)
 
                         pthread_rwlock_wrlock(&ipcpi.state_lock);
 
-                        ipcp_set_state(IPCP_SHUTDOWN);
+                        if (ipcp_get_state() == IPCP_INIT)
+                                ipcp_set_state(IPCP_NULL);
+
+                        if (ipcp_get_state() == IPCP_ENROLLED)
+                                ipcp_set_state(IPCP_SHUTDOWN);
 
                         pthread_rwlock_unlock(&ipcpi.state_lock);
                 }
@@ -154,9 +158,6 @@ static int ipcp_local_bootstrap(struct dif_config * conf)
         assert(conf);
         assert(conf->type == THIS_TYPE);
 
-        /* this IPCP doesn't need to maintain its dif_name */
-        free(conf->dif_name);
-
         pthread_rwlock_wrlock(&ipcpi.state_lock);
 
         if (ipcp_get_state() != IPCP_INIT) {
@@ -164,6 +165,9 @@ static int ipcp_local_bootstrap(struct dif_config * conf)
                 LOG_ERR("IPCP in wrong state.");
                 return -1;
         }
+
+        /* this IPCP doesn't need to maintain its dif_name */
+        free(conf->dif_name);
 
         ipcp_set_state(IPCP_ENROLLED);
 
@@ -382,8 +386,10 @@ int main(int argc, char * argv[])
 
         ipcp_fini();
 
-        pthread_cancel(local_data.sduloop);
-        pthread_join(local_data.sduloop, NULL);
+        if (ipcp_get_state() == IPCP_SHUTDOWN) {
+                pthread_cancel(local_data.sduloop);
+                pthread_join(local_data.sduloop, NULL);
+        }
 
         local_data_fini();
 
