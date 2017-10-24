351da36fb339b56b95cff52ff5d0da1ad8e20def
Sander Vrijders <sander.vrijders@ugent.be>
2017-10-24 20:49:26 +0200

    tools: Remove comment lines in CBR
    There were unnecessary comment lines in cbr.c, this removes them.
    
    Signed-off-by: Sander Vrijders <sander.vrijders@ugent.be>
    Signed-off-by: Dimitri Staessens <dimitri.staessens@ugent.be>

--- src/tools/cbr/cbr.c
+++ src/tools/cbr/cbr.c
@@ -6,8 +6,6 @@
  *    Dimitri Staessens <dimitri.staessens@ugent.be>
  *    Sander Vrijders   <sander.vrijders@ugent.be>
  *
- *
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
