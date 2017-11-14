91cbb32d3122dc9a63a5fdfc09d7c8822f95ddf6
Sander Vrijders <sander.vrijders@ugent.be>
2017-11-14 21:49:19 +0100

    build: Set CMAKE_REQUIRED_FLAGS in CompilerUtils
    This sets the cmake variable CMAKE_REQUIRED_FLAGS to the compiler
    flags that are being tested so that the linker doesn't fail when
    trying the compile flag.
    
    Signed-off-by: Sander Vrijders <sander.vrijders@ugent.be>
    Signed-off-by: Dimitri Staessens <dimitri.staessens@ugent.be>

--- cmake/CompilerUtils.cmake
+++ cmake/CompilerUtils.cmake
@@ -3,6 +3,7 @@ include(CheckCCompilerFlag)
 function(test_and_set_c_compiler_flag_global _flag)
 
   string(REGEX REPLACE "-" "_" _sflag ${_flag})
+  set(CMAKE_REQUIRED_FLAGS ${_flag})
   check_c_compiler_flag(${_flag} COMPILER_SUPPORTS_FLAG_${_sflag})
 
   if(COMPILER_SUPPORTS_FLAG_${_sflag})
