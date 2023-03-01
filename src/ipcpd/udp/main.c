dstaesse/ouroboros

commit 269f25d3bac5ab871d8044935eacc15cfeadeec6

ipcpd: refactor IPCP configuration
The ipcp configuration struct now has internal structures for the
different IPCPs and for IPCP components of the unicast IPCP.

Split the very long IPCP main loop into individual handler functions.

Signed-off-by: Dimitri Staessens <dimitri@ouroboros.rocks>
Signed-off-by: Sander Vrijders <sander@ouroboros.rocks>

src/ipcpd/udp/main.c
@@ -591,6 +591,12 @@ static void * ipcp_udp_packet_writer(void * o)
         return (void *) 1;
 }
 
+static const char * inet4_ntop(const void * addr,
+                         char *       buf)
+{
+        return inet_ntop(AF_INET, &addr, buf, INET_ADDRSTRLEN);
+}
+
 static int ipcp_udp_bootstrap(const struct ipcp_config * conf)
 {
         char ipstr[INET_ADDRSTRLEN];
@@ -602,21 +608,15 @@ static int ipcp_udp_bootstrap(const struct ipcp_config * conf)
         assert(conf->type == THIS_TYPE);
 
         ipcpi.dir_hash_algo = conf->layer_info.dir_hash_algo;
-        ipcpi.layer_name = strdup(conf->layer_info.layer_name);
-        if (ipcpi.layer_name == NULL) {
-                log_err("Failed to set layer name");
-                return -ENOMEM;
-        }
+        strcpy(ipcpi.layer_name, conf->layer_info.layer_name);
 
-        if (inet_ntop(AF_INET, &conf->ip_addr, ipstr, INET_ADDRSTRLEN)
-            == NULL) {
+        if (inet4_ntop(&conf->udp.ip_addr, ipstr) == NULL) {
                 log_err("Failed to convert IP address");
                 return -1;
         }
 
-        if (conf->dns_addr != 0) {
-                if (inet_ntop(AF_INET, &conf->dns_addr, dnsstr, INET_ADDRSTRLEN)
-                    == NULL) {
+        if (conf->udp.dns_addr != 0) {
+                if (inet4_ntop(&conf->udp.dns_addr, dnsstr) == NULL) {
                         log_err("Failed to convert DNS address");
                         return -1;
                 }
@@ -636,15 +636,15 @@ static int ipcp_udp_bootstrap(const struct ipcp_config * conf)
 
         memset((char *) &udp_data.s_saddr, 0, sizeof(udp_data.s_saddr));
         udp_data.s_saddr.sin_family      = AF_INET;
-        udp_data.s_saddr.sin_addr.s_addr = conf->ip_addr;
-        udp_data.s_saddr.sin_port        = htons(conf->port);
+        udp_data.s_saddr.sin_addr.s_addr = conf->udp.ip_addr;
+        udp_data.s_saddr.sin_port        = htons(conf->udp.port);
 
         if (bind(udp_data.s_fd, SADDR, SADDR_SIZE) < 0) {
                 log_err("Couldn't bind to %s.", ipstr);
                 goto fail_bind;
         }
 
-        udp_data.dns_addr = conf->dns_addr;
+        udp_data.dns_addr = conf->udp.dns_addr;
 
         ipcp_set_state(IPCP_OPERATIONAL);
 
@@ -670,11 +670,11 @@ static int ipcp_udp_bootstrap(const struct ipcp_config * conf)
                 }
         }
 
-        sprintf(portstr, "%d", conf->port);
+        sprintf(portstr, "%d", conf->udp.port);
 
         log_dbg("Bootstrapped IPCP over UDP with pid %d.", getpid());
         log_dbg("Bound to IP address %s.", ipstr);
-        log_dbg("Using port %u.", conf->port);
+        log_dbg("Using port %u.", conf->udp.port);
         log_dbg("DNS server address is %s.", dnsstr);
 
         return 0;
@@ -759,7 +759,7 @@ static uint32_t ddns_resolve(char *   name,
         char *   addr_str = "Address:";
         uint32_t ip_addr  = 0;
 
-        if (inet_ntop(AF_INET, &dns_addr, dnsstr, INET_ADDRSTRLEN) == NULL)
+        if (inet4_ntop(&dns_addr, dnsstr) == NULL)
                 return 0;
 
         if (pipe(pipe_fd)) {
@@ -855,14 +855,12 @@ static int ipcp_udp_reg(const uint8_t * hash)
         if (dns_addr != 0) {
                 ip_addr = udp_data.s_saddr.sin_addr.s_addr;
 
-                if (inet_ntop(AF_INET, &ip_addr,
-                              ipstr, INET_ADDRSTRLEN) == NULL) {
+                if (inet4_ntop(&ip_addr, ipstr) == NULL) {
                         free(hashstr);
                         return -1;
                 }
 
-                if (inet_ntop(AF_INET, &dns_addr,
-                              dnsstr, INET_ADDRSTRLEN) == NULL) {
+                if (inet4_ntop(&dns_addr, dnsstr) == NULL) {
                         free(hashstr);
                         return -1;
                 }
@@ -908,8 +906,7 @@ static int ipcp_udp_unreg(const uint8_t * hash)
         dns_addr = udp_data.dns_addr;
 
         if (dns_addr != 0) {
-                if (inet_ntop(AF_INET, &dns_addr, dnsstr, INET_ADDRSTRLEN)
-                    == NULL) {
+                if (inet4_ntop(&dns_addr, dnsstr) == NULL) {
                         free(hashstr);
                         return -1;
                 }
@@ -1002,13 +999,16 @@ static int ipcp_udp_flow_alloc(int             fd,
         assert(dst);
 
         if (!shim_data_dir_has(udp_data.shim_data, dst)) {
-                log_dbg("Could not resolve destination.");
+                log_err("Could not resolve destination.");
                 return -1;
         }
 
         ip_addr = (uint32_t) shim_data_dir_get_addr(udp_data.shim_data, dst);
 
-        inet_ntop(AF_INET, &ip_addr, ipstr, INET_ADDRSTRLEN);
+        if (inet4_ntop(&ip_addr, ipstr) == NULL) {
+                log_err("Could not convert IP address.");
+                return -1;
+        }
         log_dbg("Destination UDP ipcp resolved at %s.", ipstr);
 
         memset((char *) &r_saddr, 0, sizeof(r_saddr));

src/ipcpd/broadcast/main.c
@@ -56,36 +56,24 @@ struct ipcp ipcpi;
 
 static int initialize_components(const struct ipcp_config * conf)
 {
-        ipcpi.layer_name = strdup(conf->layer_info.layer_name);
-        if (ipcpi.layer_name == NULL) {
-                log_err("Failed to set layer name.");
-                goto fail_layer_name;
-        }
-
+        strcpy(ipcpi.layer_name, conf->layer_info.layer_name);
         ipcpi.dir_hash_algo = conf->layer_info.dir_hash_algo;
 
         assert(ipcp_dir_hash_len() != 0);
 
         if (dt_init()) {
                 log_err("Failed to initialize forwarding component.");
-                goto fail_dt;
+                return -1;
         }
 
         ipcp_set_state(IPCP_INIT);
 
         return 0;
-
- fail_dt:
-        free(ipcpi.layer_name);
- fail_layer_name:
-        return -1;
 }
 
 static void finalize_components(void)
 {
         dt_fini();
-
-        free(ipcpi.layer_name);
 }
 
 static int start_components(void)

src/ipcpd/local/main.c
@@ -145,11 +145,7 @@ static int ipcp_local_bootstrap(const struct ipcp_config * conf)
         assert(conf->type == THIS_TYPE);
 
         ipcpi.dir_hash_algo = conf->layer_info.dir_hash_algo;
-        ipcpi.layer_name = strdup(conf->layer_info.layer_name);
-        if (ipcpi.layer_name == NULL) {
-                log_err("Failed to set layer name");
-                return -ENOMEM;
-        }
+        strcpy(ipcpi.layer_name,conf->layer_info.layer_name);
 
         ipcp_set_state(IPCP_OPERATIONAL);
 

src/ipcpd/unicast/main.c
@@ -59,18 +59,13 @@ struct ipcp ipcpi;
 
 static int initialize_components(const struct ipcp_config * conf)
 {
-        ipcpi.layer_name = strdup(conf->layer_info.layer_name);
-        if (ipcpi.layer_name == NULL) {
-                log_err("Failed to set layer name.");
-                goto fail_layer_name;
-        }
-
+        strcpy(ipcpi.layer_name, conf->layer_info.layer_name);
         ipcpi.dir_hash_algo = conf->layer_info.dir_hash_algo;
 
         assert(ipcp_dir_hash_len() != 0);
 
-        if (addr_auth_init(conf->addr_auth_type,
-                           &conf->addr_size)) {
+        if (addr_auth_init(conf->unicast.addr_auth_type,
+                           &conf->unicast.dt.addr_size)) {
                 log_err("Failed to init address authority.");
                 goto fail_addr_auth;
         }
@@ -83,15 +78,12 @@ static int initialize_components(const struct ipcp_config * conf)
 
         log_dbg("IPCP got address %" PRIu64 ".", ipcpi.dt_addr);
 
-        if (ca_init(conf->cong_avoid)) {
+        if (ca_init(conf->unicast.cong_avoid)) {
                 log_err("Failed to initialize congestion avoidance.");
                 goto fail_ca;
         }
 
-        if (dt_init(conf->routing_type,
-                    conf->addr_size,
-                    conf->eid_size,
-                    conf->max_ttl)) {
+        if (dt_init(conf->unicast.dt)) {
                 log_err("Failed to initialize data transfer component.");
                 goto fail_dt;
         }
@@ -119,8 +111,6 @@ static int initialize_components(const struct ipcp_config * conf)
  fail_ca:
         addr_auth_fini();
  fail_addr_auth:
-        free(ipcpi.layer_name);
- fail_layer_name:
         return -1;
 }
 
@@ -135,8 +125,6 @@ static void finalize_components(void)
         ca_fini();
 
         addr_auth_fini();
-
-        free(ipcpi.layer_name);
 }
 
 static int start_components(void)

include/ouroboros/ipcp.h
@@ -29,10 +29,7 @@
 
 #define LAYER_NAME_SIZE 255
 
-/*
- * NOTE: the IRMd uses this order to select an IPCP
- * for flow allocation.
- */
+/* NOTE: The IRMd uses this order to select an IPCP for flow allocation. */
 enum ipcp_type {
         IPCP_LOCAL = 0,
         IPCP_UNICAST,
@@ -66,6 +63,31 @@ enum pol_dir_hash {
         DIR_HASH_SHA3_512
 };
 
+struct dt_config {
+        uint8_t          addr_size;
+        uint8_t          eid_size;
+        uint8_t          max_ttl;
+        enum pol_routing routing_type;
+};
+
+/* IPCP configuration */
+struct uni_config {
+        struct dt_config    dt;
+        enum pol_addr_auth  addr_auth_type;
+        enum pol_cong_avoid cong_avoid;
+};
+
+struct eth_config {
+        char *   dev;
+        uint16_t ethertype; /* DIX only*/
+};
+
+struct udp_config {
+        uint32_t ip_addr;
+        uint32_t dns_addr;
+        uint16_t port;
+};
+
 /* Info reported back to the IRMd about the layer on enrollment */
 struct layer_info {
         char layer_name[LAYER_NAME_SIZE + 1];
@@ -74,29 +96,14 @@ struct layer_info {
 
 /* Structure to configure the first IPCP */
 struct ipcp_config {
-        struct layer_info   layer_info;
+        struct layer_info layer_info;
+        enum ipcp_type    type;
 
-        enum ipcp_type      type;
-
-        /* Unicast */
-        uint8_t             addr_size;
-        uint8_t             eid_size;
-        uint8_t             max_ttl;
-
-        enum pol_addr_auth  addr_auth_type;
-        enum pol_routing    routing_type;
-        enum pol_cong_avoid cong_avoid;
-
-        /* UDP */
-        uint32_t            ip_addr;
-        uint32_t            dns_addr;
-        uint16_t            port;
-
-        /* Ethernet */
-        char *              dev;
-
-        /* Ethernet DIX */
-        uint16_t            ethertype;
+        union {
+                struct uni_config unicast;
+                struct udp_config udp;
+                struct eth_config eth;
+        };
 };
 
 #endif /* OUROBOROS_IPCP_H */

include/ouroboros/sockets.h.in
@@ -30,6 +30,10 @@
 #include "ipcp_config.pb-c.h"
 typedef IpcpConfigMsg ipcp_config_msg_t;
 typedef LayerInfoMsg layer_info_msg_t;
+typedef DtConfigMsg dt_config_msg_t;
+typedef EthConfigMsg eth_config_msg_t;
+typedef UdpConfigMsg udp_config_msg_t;
+typedef UniConfigMsg uni_config_msg_t;
 
 #include "irmd_messages.pb-c.h"
 typedef IrmMsg irm_msg_t;

src/ipcpd/common/enroll.c
@@ -74,7 +74,9 @@ static int send_rcv_enroll_msg(int fd)
         ssize_t         delta_t;
         struct timespec t0;
         struct timespec rtt;
-
+#ifdef BUILD_IPCP_UNICAST
+        uni_config_msg_t * uni_cfg_msg;
+#endif
         req.code = ENROLL_CODE__ENROLL_REQ;
 
         len = enroll_msg__get_packed_size(&req);
@@ -132,12 +134,14 @@ static int send_rcv_enroll_msg(int fd)
                reply->conf->layer_info->layer_name);
         enroll.conf.type           = reply->conf->ipcp_type;
 #ifdef BUILD_IPCP_UNICAST
-        enroll.conf.addr_size      = reply->conf->addr_size;
-        enroll.conf.eid_size       = reply->conf->eid_size;
-        enroll.conf.max_ttl        = reply->conf->max_ttl;
-        enroll.conf.addr_auth_type = reply->conf->addr_auth_type;
-        enroll.conf.routing_type   = reply->conf->routing_type;
-        enroll.conf.cong_avoid     = reply->conf->cong_avoid;
+        uni_cfg_msg = reply->conf->unicast;
+
+        enroll.conf.unicast.dt.addr_size    = uni_cfg_msg->dt->addr_size;
+        enroll.conf.unicast.dt.eid_size     = uni_cfg_msg->dt->eid_size;
+        enroll.conf.unicast.dt.max_ttl      = uni_cfg_msg->dt->max_ttl;
+        enroll.conf.unicast.dt.routing_type = uni_cfg_msg->dt->routing_type;
+        enroll.conf.unicast.addr_auth_type  = uni_cfg_msg->addr_auth_type;
+        enroll.conf.unicast.cong_avoid      = uni_cfg_msg->cong_avoid;
 #endif
         enroll.conf.layer_info.dir_hash_algo
                 = reply->conf->layer_info->dir_hash_algo;
@@ -151,6 +155,10 @@ static ssize_t enroll_pack(uint8_t ** buf)
         enroll_msg_t      msg        = ENROLL_MSG__INIT;
         ipcp_config_msg_t config     = IPCP_CONFIG_MSG__INIT;
         layer_info_msg_t  layer_info = LAYER_INFO_MSG__INIT;
+#ifdef BUILD_IPCP_UNICAST
+        dt_config_msg_t   dt_cfg_msg  = DT_CONFIG_MSG__INIT;
+        uni_config_msg_t  uni_cfg_msg = UNI_CONFIG_MSG__INIT;
+#endif
         struct timespec   now;
         ssize_t           len;
 
@@ -161,27 +169,23 @@ static ssize_t enroll_pack(uint8_t ** buf)
         msg.t_sec      = now.tv_sec;
         msg.has_t_nsec = true;
         msg.t_nsec     = now.tv_nsec;
-        msg.conf       = &config;
 
-        config.ipcp_type          = enroll.conf.type;
+        config.ipcp_type           = enroll.conf.type;
 #ifdef BUILD_IPCP_UNICAST
-        config.has_addr_size      = true;
-        config.addr_size          = enroll.conf.addr_size;
-        config.has_eid_size       = true;
-        config.eid_size           = enroll.conf.eid_size;
-        config.has_max_ttl        = true;
-        config.max_ttl            = enroll.conf.max_ttl;
-        config.has_addr_auth_type = true;
-        config.addr_auth_type     = enroll.conf.addr_auth_type;
-        config.has_routing_type   = true;
-        config.routing_type       = enroll.conf.routing_type;
-        config.has_cong_avoid     = true;
-        config.cong_avoid         = enroll.conf.cong_avoid;
+        dt_cfg_msg.addr_size       = enroll.conf.unicast.dt.addr_size;
+        dt_cfg_msg.eid_size        = enroll.conf.unicast.dt.eid_size;
+        dt_cfg_msg.max_ttl         = enroll.conf.unicast.dt.max_ttl;
+        dt_cfg_msg.routing_type    = enroll.conf.unicast.dt.routing_type;
+        uni_cfg_msg.dt             = &dt_cfg_msg;
+        uni_cfg_msg.addr_auth_type = enroll.conf.unicast.addr_auth_type;
+        uni_cfg_msg.cong_avoid     = enroll.conf.unicast.cong_avoid;
+        config.unicast             = &uni_cfg_msg;
 #endif
-        config.layer_info         = &layer_info;
-
         layer_info.layer_name     = (char *) enroll.conf.layer_info.layer_name;
         layer_info.dir_hash_algo  = enroll.conf.layer_info.dir_hash_algo;
+
+        config.layer_info = &layer_info;
+        msg.conf          = &config;
 
         len = enroll_msg__get_packed_size(&msg);
 

src/ipcpd/eth/eth.c
@@ -1281,31 +1281,27 @@ static int eth_ipcp_bootstrap(const struct ipcp_config * conf)
         assert(conf->type == THIS_TYPE);
 
         ipcpi.dir_hash_algo = conf->layer_info.dir_hash_algo;
-        ipcpi.layer_name = strdup(conf->layer_info.layer_name);
-        if (ipcpi.layer_name == NULL) {
-                log_err("Failed to set layer name");
-                return -ENOMEM;
-        }
+        strcpy(ipcpi.layer_name, conf->layer_info.layer_name);
 
-        if (conf->dev == NULL) {
+        if (conf->eth.dev == NULL) {
                 log_err("Device name is NULL.");
                 return -1;
         }
 
-        if (strlen(conf->dev) >= IFNAMSIZ) {
-                log_err("Invalid device name: %s.", conf->dev);
+        if (strlen(conf->eth.dev) >= IFNAMSIZ) {
+                log_err("Invalid device name: %s.", conf->eth.dev);
                 return -1;
         }
 
         memset(&ifr, 0, sizeof(ifr));
-        strcpy(ifr.ifr_name, conf->dev);
+        strcpy(ifr.ifr_name, conf->eth.dev);
 
 #ifdef BUILD_ETH_DIX
-        if (conf->ethertype < 0x0600 || conf->ethertype == 0xFFFF) {
+        if (conf->eth.ethertype < 0x0600 || conf->eth.ethertype == 0xFFFF) {
                 log_err("Invalid Ethertype.");
                 return -1;
         }
-        eth_data.ethertype = htons(conf->ethertype);
+        eth_data.ethertype = htons(conf->eth.ethertype);
 #endif
 
 #if defined(__FreeBSD__) || defined(__APPLE__)
@@ -1315,9 +1311,9 @@ static int eth_ipcp_bootstrap(const struct ipcp_config * conf)
         }
 
         for (ifa = ifaddr, idx = 0; ifa != NULL; ifa = ifa->ifa_next, ++idx) {
-                if (strcmp(ifa->ifa_name, conf->dev))
+                if (strcmp(ifa->ifa_name, conf->eth.dev))
                         continue;
-                log_dbg("Interface %s found.", conf->dev);
+                log_dbg("Interface %s found.", conf->eth.dev);
 
     #if defined(HAVE_NETMAP) || defined(HAVE_BPF)
                 memcpy(eth_data.hw_addr,
@@ -1352,7 +1348,8 @@ static int eth_ipcp_bootstrap(const struct ipcp_config * conf)
         log_dbg("Device MTU is %d.", ifr.ifr_mtu);
 
         eth_data.mtu = MIN((int) ETH_MTU_MAX, ifr.ifr_mtu);
-        if (memcmp(conf->dev, "lo", 2) == 0 && eth_data.mtu > IPCP_ETH_LO_MTU) {
+        if (memcmp(conf->eth.dev, "lo", 2) == 0 &&
+                   eth_data.mtu > IPCP_ETH_LO_MTU) {
                 log_dbg("Using loopback interface. MTU restricted to %d.",
                          IPCP_ETH_LO_MTU);
                 eth_data.mtu = IPCP_ETH_LO_MTU;
@@ -1376,7 +1373,7 @@ static int eth_ipcp_bootstrap(const struct ipcp_config * conf)
 
         close(skfd);
 
-        idx = if_nametoindex(conf->dev);
+        idx = if_nametoindex(conf->eth.dev);
         if (idx == 0) {
                 log_err("Failed to retrieve interface index.");
                 return -1;
@@ -1386,7 +1383,7 @@ static int eth_ipcp_bootstrap(const struct ipcp_config * conf)
 
 #if defined(HAVE_NETMAP)
         strcpy(ifn, "netmap:");
-        strcat(ifn, conf->dev);
+        strcat(ifn, conf->eth.dev);
 
         eth_data.nmd = nm_open(ifn, NULL, 0, NULL);
         if (eth_data.nmd == NULL) {
@@ -1526,7 +1523,7 @@ static int eth_ipcp_bootstrap(const struct ipcp_config * conf)
 
 #if defined(BUILD_ETH_DIX)
         log_dbg("Bootstrapped IPCP over DIX Ethernet with pid %d "
-                "and Ethertype 0x%X.", getpid(), conf->ethertype);
+                "and Ethertype 0x%X.", getpid(), conf->eth.ethertype);
 #elif defined(BUILD_ETH_LLC)
         log_dbg("Bootstrapped IPCP over Ethernet with LLC with pid %d.",
                 getpid());

commit f38936e62cfcf8f0add97c4bea3b5662f3e07a19

ipcpd: Fix address conversion in ipcpd-udp
Bug introduced in 269f25d3. The wrong pointer was passed to inet_ntop.

Signed-off-by: Dimitri Staessens <dimitri@ouroboros.rocks>
Signed-off-by: Sander Vrijders <sander@ouroboros.rocks>

src/ipcpd/udp/main.c
@@ -592,9 +592,9 @@ static void * ipcp_udp_packet_writer(void * o)
 }
 
 static const char * inet4_ntop(const void * addr,
-                         char *       buf)
+                               char *       buf)
 {
-        return inet_ntop(AF_INET, &addr, buf, INET_ADDRSTRLEN);
+        return inet_ntop(AF_INET, addr, buf, INET_ADDRSTRLEN);
 }
 
 static int ipcp_udp_bootstrap(const struct ipcp_config * conf)